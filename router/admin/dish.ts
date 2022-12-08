e4afd9c15da966556a556238d07e67de33d44beb
lainxin <3598556808@qq.com>
2022-12-08 20:28:22 +0800

    菜品

--- router/admin/dish.ts
+++ router/admin/dish.ts
@@ -6,9 +6,9 @@ import flq from "../../SQLConnect";
 
 const router = new Router({ prefix: '/dish' })
 
-const dish_cate = flq.from('dishes_cate')
+// const dish_cate = flq.from('dishes_cate')
 const dish_unit = flq.from('dishes_unit')
-const dish_data = flq.from('dishes_data')
+// const dish_data = flq.from('dishes_data')
 
 
 // 菜品单位
@@ -19,17 +19,17 @@ router.post('/unit', validator(dishUnitRules),async (ctx) => {
     const { label } = ctx.data
     const { uid } = ctx.state.user
     // 判断单位名是否重复
-    const res = await dish_unit.where({ uid, label }).first()
-    if(res) return ctx.error('单位名重复', 202)
+    const res = await dish_unit.where({  label,uid }).first()
+    if(res) return ctx.error('单位名重复', 302)
     // 添加单位
-    await dish_unit.value({ ...ctx.data, uid }).add()
+    await dish_unit.value({ ...ctx.data,uid }).add()
     ctx.success()
 })
 
 
 // 获取单位
 router.get('/unit', async (ctx) => {
-    
+    ctx.success()
 })
 
 
